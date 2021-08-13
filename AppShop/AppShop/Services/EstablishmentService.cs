af6932c0ecb62505b58da17d5b70409445741ea6
kennedyrocha <kennedy279972@gmailcom>
2021-08-13 07:30:00 -0400

    Adicionar arquivos de projeto.

---
+++ AppShop/AppShop/Services/EstablishmentService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppShop.Models;
+using AppShop.Libraries.Enums;
+
+namespace AppShop.Services
+{
+    public class EstablishmentService
+    {
+        public List<Establishment> GetEstablishments()
+        {
+            var establishments = new List<Establishment>()
+            {
+                new Establishment()
+                {
+                    Id = 1,
+                    Type = EstablishmentType.Store,
+                    Logo = "https://api-dev.orez.com.br/swagger/ui/images/logo_small-png",
+                    Name = "Renner",
+                    Description = "Primeira descrição",
+                    Address = "Endereço 1 rua 4",
+                    Phone = "(66) 99662-5248"
+                },
+                new Establishment()
+                {
+                    Id = 2,
+                    Type = EstablishmentType.Store,
+                    Logo = "https://www.udemy.com/staticx/udemy/images/v7/logo-udemy.svg",
+                    Name = "Kodak",
+                    Description = "Segunda descrição",
+                    Address = "Endereço 2 Avenida 4",
+                    Phone = "(66) 19662-5248"
+                },
+                new Establishment()
+                {
+                    Id = 1,
+                    Type = EstablishmentType.Restaurant,
+                    Logo = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png",
+                    Name = "Kadri",
+                    Description = "Terceira descrição",
+                    Address = "Endereço 3 rua 5",
+                    Phone = "(66) 29662-5248"
+                },
+                new Establishment()
+                {
+                    Id = 1,
+                    Type = EstablishmentType.Restaurant,
+                    Logo = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png",
+                    Name = "Kadri23",
+                    Description = "Terceira descrição23",
+                    Address = "Endereço 3 rua 523",
+                    Phone = "(66) 29662-5248"
+                }
+            };
+            return establishments;
+        }
+    }
+}
---
+++ AppShop/AppShop/ViewModels/StoresViewModel.cs
@@ -0,0 +1,34 @@
+﻿using AppShop.Models;
+using AppShop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using AppShop.Libraries.Enums;
+
+namespace AppShop.ViewModels
+{
+    public class StoresViewModel
+    {
+        public String SearchWord { get; set; }
+        public ICommand SearchCommand { get; set; }
+        public List<Establishment> Establishments { get; set; }
+
+        public StoresViewModel()
+        {
+            SearchCommand = new Command(Search);
+
+            var allEstablishment = new EstablishmentService().GetEstablishments();
+            var allStores = allEstablishment.Where(a => a.Type == EstablishmentType.Store);
+
+            Establishments = allEstablishment;
+        }
+
+        private void Search()
+        {
+
+        }
+    }
+}
